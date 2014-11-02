790e0f7a9a21b3862fc7bd477d21ae4706dbef0f
Alex <alexking124@gmail.com>
2014-11-02 13:50:03 -0800

    Accelerometer monitoring and actions implemented.

--- Active Player/ViewController.m
+++ Active Player/ViewController.m
@@ -15,11 +15,22 @@
 @property (nonatomic) float sensitivity;
 @property (nonatomic) float responsiveness;
 
+@property (strong, nonatomic) NSNumber *magnitude;
+@property (nonatomic) float newMagnitude;
+@property (nonatomic) float lastMagnitude;
+
+@property (strong, nonatomic) NSMutableArray *accelerationData;
+@property (strong, nonatomic) NSDate *lastUpdateTime;
+
 @property (strong, nonatomic) CMMotionManager *motionManager;
+@property (strong, nonatomic) UISlider *volumeViewSlider;
+
+@property (strong, nonatomic) NSOperationQueue *backgroundQueue;
 
 @end
 
 @implementation ViewController
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -28,7 +39,36 @@
     [self.activeVolumeSlider setValue:0.7];
     [self.restVolumeSlider setValue:0.15];
     [self.sensitivitySlider setValue:0.85];
-    [self.responsivenessSlider setValue:0.3];
+    [self.responsivenessSlider setMinimumValue:1.0];
+    [self.responsivenessSlider setMaximumValue:10.0];
+    [self.responsivenessSlider setValue:3.0];
+    [self.controlSwitch setOn:NO];
+    
+    self.activeVolume = self.activeVolumeSlider.value;
+    self.restVolume = self.restVolumeSlider.value;
+    self.sensitivity = self.sensitivitySlider.value;
+    self.responsiveness = self.responsivenessSlider.value;
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    [self.motionManager setDeviceMotionUpdateInterval:1.0/60.0];
+    
+    self.accelerationData = [[NSMutableArray alloc] init];
+    
+    MPVolumeView *volumeView = [[MPVolumeView alloc] init];
+    for (UIView *view in [volumeView subviews]){
+        if ([view.class.description isEqualToString:@"MPVolumeSlider"]){
+            self.volumeViewSlider = (UISlider*)view;
+            break;
+        }
+    }
+    
+    self.magnitude = [[NSNumber alloc] init];
+    self.newMagnitude = 0.0;
+    self.lastMagnitude = 0.0;
+    
+    self.lastUpdateTime = [NSDate date];
+    
+    self.backgroundQueue = [[NSOperationQueue alloc] init];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -39,9 +79,8 @@
 
 -(IBAction)sliderChangedValue:(UISlider *)sender
 {
-    if (sender == self.activeVolumeSlider) {
-        MPMusicPlayerController *musicPlayer = [MPMusicPlayerController systemMusicPlayer];
-        [musicPlayer setVolume:sender.value];
+    if (sender == self.activeVolumeSlider)
+    {
         self.activeVolume = sender.value;
     }
     else if (sender == self.restVolumeSlider)
@@ -54,7 +93,7 @@
     }
     else if (sender == self.responsivenessSlider)
     {
-        self.responsiveness = sender.value;
+        self.responsiveness = round(sender.value);
     }
 }
 
@@ -62,12 +101,65 @@
 {
     if ([self.controlSwitch isOn])
     {
-        [self.motionManager startAccelerometerUpdates];
+        [self.motionManager startDeviceMotionUpdatesToQueue:self.backgroundQueue withHandler:^ (CMDeviceMotion *motionData, NSError *error) {
+            [self receivedNewMotionUpdate:motionData];
+        }];
     }
     else
     {
-        [self.motionManager stopAccelerometerUpdates];
+        [self.motionManager stopDeviceMotionUpdates];
     }
+}
+
+- (void)receivedNewMotionUpdate:(CMDeviceMotion*)motionData
+{
+    CMAcceleration newAcceleration = [motionData userAcceleration];
+    float x = newAcceleration.x;
+    float y = newAcceleration.y;
+    float z = newAcceleration.z;
+    //NSLog(@"Got acceleration with x: %f, y: %f, z: %f", x, y, z);
+    
+    self.lastMagnitude = self.newMagnitude;
+    self.newMagnitude = sqrt(x * x + y * y + z * z);
+    float delta = self.newMagnitude - self.lastMagnitude;
+    self.magnitude = [NSNumber numberWithDouble:fabsf(self.magnitude.floatValue * 0.9 + delta)]; //perform low-cut filter
+    [self.accelerationData addObject:self.magnitude];
+    
+    //NSLog(@"Magnitude of acceleration: %f", self.newMagnitude);
+    
+    if (-[self.lastUpdateTime timeIntervalSinceNow] >= 2 / self.responsiveness)
+    {
+        if ([self average:self.accelerationData] > (0.11 * self.sensitivity))
+        {
+            [self.volumeViewSlider setValue:self.activeVolume animated:NO];
+            [self.volumeViewSlider sendActionsForControlEvents:UIControlEventTouchUpInside];
+        }
+        else
+        {
+            [self.volumeViewSlider setValue:self.restVolume animated:NO];
+            [self.volumeViewSlider sendActionsForControlEvents:UIControlEventTouchUpInside];
+        }
+        self.lastUpdateTime = [NSDate date];
+        [self.accelerationData removeAllObjects];
+    }
+    
+}
+
+- (float)average:(NSMutableArray*)array
+{
+    float average = [self sum:array] / array.count;
+    return average;
+}
+
+- (float)sum:(NSMutableArray*)array
+{
+    float total = 0.0;
+    NSNumber *number;
+    for (number in array)
+    {
+        total = total + number.floatValue;
+    }
+    return total;
 }
 
 @end
